b297d889adac18b222d96909bcef9de23c105aca
Yasmin vitória costa <99223801+YASMINCOS@users.noreply.github.com>
2022-11-02 09:47:37 -0300

    Delete TelaLoginTypescitpt/youtube-curso-react-materialui-typescript-c983f848158156f8ef68a4d389380db3607eb13b directory

--- TelaLoginTypescitpt/youtube-curso-react-materialui-typescript-c983f848158156f8ef68a4d389380db3607eb13b/src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './pessoas/ListagemDePessoas';
-export * from './pessoas/DetalheDePessoas';
-export * from './cidades/ListagemDeCidades';
-export * from './cidades/DetalheDeCidades';
-export * from './dashboard/Dashboard';
